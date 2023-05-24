919ba439a9b485a042574f3f36da83fe99ff475d
David alves <107942209+DavidAlves28@users.noreply.github.com>
2023-05-24 17:30:43 -0300

    Update next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["avatars.githubusercontent.com", "https://nlw-spacetime-server-6dm1f4x4o-davidalves28.vercel.app/"],
+    domains: ["avatars.githubusercontent.com", "https://nlw-spacetime-server-6dm1f4x4o-davidalves28.vercel.app"],
   },
 };
 
