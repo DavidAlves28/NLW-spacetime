2f4dc81d9f30e4d087900f308179bb2438991335
david alves <dav.alvesc28@gmail.com>
2023-05-24 17:06:00 -0300

    troca de base url

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-baseURL:'https://nlwe-server.vercel.app'
+baseURL:'https://nlw-spacetime-server-6dm1f4x4o-davidalves28.vercel.app'
 });
