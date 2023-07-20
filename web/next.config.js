f7b714ca626b1e3a28cfd79820e795f9a0bf952a
David alves <107942209+DavidAlves28@users.noreply.github.com>
2023-07-19 21:38:17 -0300

    Revert "troca de base url"

--- web/next.config.js
+++ web/next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-
-    domains: ["avatars.githubusercontent.com", "https://localhost:3003"],
     domains: ["avatars.githubusercontent.com", "192.168.0.9"],
-
   },
 };
 
